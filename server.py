a0d3ad2aead63c455253b5d092fdfd4846bdc3f5
Pranjal Jain <pranjalworkon@gmail.com>
2025-12-17 22:49:12 +0530

    feat: added the rate limit

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from config import settings
 from services.markdown_report import generate_markdown, save_markdown_report
 from services.trade_search import fetch_market_news
 from services.ai_service import analyze_sector as ai_analyze_sector
+from security.rate_limit import limiter
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
 
-app=FastAPI()
+app = FastAPI()
+
+# Adding rate limiter to app
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 # Simple HTTP endpoint
 @app.get("/")
@@ -16,7 +23,8 @@ async def home():
 
 
 @app.get("/analyze/{sector}")
-async def analyze_sector(sector: str):
+@limiter.limit(f"{settings.RATE_LIMIT}/minute")
+async def analyze_sector(request: Request, sector: str):
     # Fetch market news
     raw_data = await fetch_market_news(sector)
     
